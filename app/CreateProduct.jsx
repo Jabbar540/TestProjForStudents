d3b66a45df1267b047eb17fa7a326c387c54a9a8
Jabbar Ahmad <jabbarahmad540@gmail.com>
2024-07-03 09:36:33 +0500

    compelte

--- app/CreateProduct.jsx
+++ app/CreateProduct.jsx
@@ -1,17 +1,39 @@
 import React, { useState } from 'react';
 import { View, StyleSheet, Text, ScrollView, TextInput,Image, TouchableOpacity } from 'react-native';
 import LayoutWrapper from '../components/LayoutWrapper';
+import CommonButton from '../components/CommonButton';
 import { Stack } from 'expo-router'; 
+import { Entypo } from '@expo/vector-icons';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function CreateProduct() {
   const [title, setTitle] = useState('');
   const [price, setPrice] = useState('');
   const [description, setDescription] = useState('');
+  const [image, setImage] = useState([]);
 
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsMultipleSelection:true
+    });
+
+    if (!result.canceled) {
+    //   setImage(result.assets);
+      setImage([...image,...result.assets]);
+    }
+  };
   
+  const filterImage=(arg)=>{
+    const newImage=[...image]
+    const filterImage=newImage.filter(i=>i.uri !== arg.uri)
+
+    setImage(filterImage)
+  }
 
   return (
-    <LayoutWrapper backgroundColor={'orange'} headerText={""} textColor='white'>
+    <LayoutWrapper backgroundColor={'green'} headerText={""} textColor='white'>
       <Stack.Screen
         options={{
           headerShown: false,
@@ -23,6 +45,22 @@ export default function CreateProduct() {
       <ScrollView>       
         <View style={styles.head}>
           <Text style={styles.text}>Add a Product</Text>
+          <View style={{marginBottom:10,flexDirection:'row'}}>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+               <TouchableOpacity onPress={pickImage} style={[styles.imagePicker]}>
+                 <Entypo name='images' size={75}/>
+               </TouchableOpacity>
+               {image.length > 0 && image.map((i)=>(
+                <View>
+                   <Image source={{ uri: i.uri }} style={styles.imagePicker} />
+                   <TouchableOpacity onPress={()=>filterImage(i)} style={styles.cross}>
+                     <Entypo name='circle-with-cross' size={25} color={'red'}/>
+                   </TouchableOpacity>
+                </View>
+                 )) 
+             }
+            </ScrollView>
+          </View>
           <Text style={styles.headline}>Title</Text>
           <TextInput
             style={styles.input}
@@ -45,9 +83,7 @@ export default function CreateProduct() {
             placeholder="Add a Description"
             
           />
-          <TouchableOpacity>
-            <Text style={styles.btn}>Save Product</Text>
-          </TouchableOpacity>
+          <CommonButton text={'Save'}/>
         </View>
       </ScrollView>
     </LayoutWrapper>
@@ -124,4 +160,19 @@ const styles = StyleSheet.create({
     marginRight: 40,
     borderRadius: 10,
   },
+  imagePicker:{
+    borderWidth:1,
+    borderRadius:12,
+    width:150,
+    height:150,
+    justifyContent:'center',
+    alignItems:'center',
+    marginLeft:20
+  },
+  cross:{
+    position:'absolute',
+    top:0,
+    right:0,
+    zIndex:1000000000,
+  }
 });
--- components/CommonButton.jsx
+++ components/CommonButton.jsx
@@ -1,13 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 
-export default function CommonButton({children}) {
+export default function CommonButton({onPress,text,btnStyle={},textStyle={}}) {
   return (
-    <View>
-      <Text>CommonButton</Text>
-      {children}
-    </View>
+    <TouchableOpacity style={[styles.container,btnStyle]} onPress={onPress}>
+      <Text style={[styles.textStyle,textStyle]}>{text}</Text>
+    </TouchableOpacity>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    width:'92%',
+    backgroundColor:'green',
+    padding:15,
+    alignSelf:'center',
+    borderRadius:16,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  textStyle:{
+    color:'white',
+    fontSize:25,
+    fontWeight:'700'
+  }
+})
